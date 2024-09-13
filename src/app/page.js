4b08bdf703961550b325968d330cd081ec999a71
Aditya-Coomar <adi.coomar04@gmail.com>
2024-09-13 13:26:48 +0530

    chore: comment out auth decode

--- src/app/page.js
+++ src/app/page.js
@@ -12,24 +12,25 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState("");
 
-  useEffect(() => {
-    const decodeToken = async () => {
-      try {
-        const decoded = await decode({
-          token: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiMmtiOVRkOG5FWS1pX2hKNldRbVF1ZkV6V0VXRzF1Szh1cnVnQlpZMmhPcWZLTEJoRlNHR1VURk5RNDVRZHRXR0p5WXpiMHV1YWMxMGkzWDB6U0Vic0EifQ..ZfJoSCssC2qjLIkWNlvzmA.lI-z99yWd94XPN1e9iAYB1C6fxwP7KWq-QU55i9A7daDfgO5qOAmBGwIgEDD4BJXhvZugrFid69LNUfmBUp76nTYuf7mFN2RMsTpw8qZI2bu90u6n19SigTeu58CQmmIJW4LB0UBVtwxoYGkl-5g_QQ4QFYsexl03vdFnCJsc8JmYL7zpqRGOkCHaKVOPvdcPqoqplc7g-vE97FEjaH0v1jPb0s6Fvh-LwhV9hUVARWRk43lJyxJhz-izatwbANEioXmX7rDqHJGTvEaJeqzN_3QjNBzSc6ry3OGUUj-Gz_ExneqR46cSGVd4SnTY7rQTc7q0bLsAx2j-6WqhHDEjA.NqGaRnKzXfaHjjcQktTjAKGMDa5oM2Q9qDyjI7EVwVo",
-          salt: "__Secure-authjs.session-token",
-          secret: "0MI/SmxJyhIk2GPSoE5h3es2h7rxTudrQpmOLilwd9w=",
-        });
-        console.log(decoded);
-      } catch (error) {
-        console.error("Error decoding token:", error);
-      }
-    };
+  {/*
+    useEffect(() => {
+      const decodeToken = async () => {
+        try {
+          const decoded = await decode({
+            token:
+              "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiMmtiOVRkOG5FWS1pX2hKNldRbVF1ZkV6V0VXRzF1Szh1cnVnQlpZMmhPcWZLTEJoRlNHR1VURk5RNDVRZHRXR0p5WXpiMHV1YWMxMGkzWDB6U0Vic0EifQ..ZfJoSCssC2qjLIkWNlvzmA.lI-z99yWd94XPN1e9iAYB1C6fxwP7KWq-QU55i9A7daDfgO5qOAmBGwIgEDD4BJXhvZugrFid69LNUfmBUp76nTYuf7mFN2RMsTpw8qZI2bu90u6n19SigTeu58CQmmIJW4LB0UBVtwxoYGkl-5g_QQ4QFYsexl03vdFnCJsc8JmYL7zpqRGOkCHaKVOPvdcPqoqplc7g-vE97FEjaH0v1jPb0s6Fvh-LwhV9hUVARWRk43lJyxJhz-izatwbANEioXmX7rDqHJGTvEaJeqzN_3QjNBzSc6ry3OGUUj-Gz_ExneqR46cSGVd4SnTY7rQTc7q0bLsAx2j-6WqhHDEjA.NqGaRnKzXfaHjjcQktTjAKGMDa5oM2Q9qDyjI7EVwVo",
+            salt: "__Secure-authjs.session-token",
+            secret: "0MI/SmxJyhIk2GPSoE5h3es2h7rxTudrQpmOLilwd9w=",
+          });
+          console.log(decoded);
+        } catch (error) {
+          console.error("Error decoding token:", error);
+        }
+      };
 
-    decodeToken();
-  }, []);
-
-
+      decodeToken();
+    }, []);
+  */}
 
   useEffect(() => {
     const storedUsername = sessionStorage.getItem("username");
@@ -38,8 +39,6 @@ export default function Home() {
       return;
     }
     setUsername(storedUsername);
-
-
 
     const fetchProjects = async () => {
       try {
