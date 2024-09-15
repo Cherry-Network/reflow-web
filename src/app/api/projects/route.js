dc9c792c6d7744896e5bb2e17a0cde524f2f01d5
Deva04-pvt <devanand.pvt.04@gmail.com>
2024-09-15 18:13:29 +0530

    fix: removed deprecated node features

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -2,10 +2,7 @@
 import { MongoClient, ObjectId } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(uri);
 
 export async function POST(req) {
   try {
