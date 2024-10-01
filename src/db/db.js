8d88681785ffd1dff167ffafc2cf25affe362fe9
Deva04-pvt <devanand.pvt.04@gmail.com>
2024-10-01 19:05:32 +0530

    fix: modified db schema

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
 
 const deviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  seriel_no: { type: String, required: true, unique: true }, // Ensure uniqueness
+  seriel_no: { type: String, required: true, unique: true },
 });
+
+
+const fullDeviceSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  seriel_no: { type: String, required: true },
+  activation_code: { type: String, required: true },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
+});
+
 
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  devices: [{ type: mongoose.Schema.Types.ObjectId, ref: "Device" }],
+  devices: [fullDeviceSchema],
   owner: { type: String, required: true },
 });
 
@@ -18,6 +26,7 @@ const userSchema = new mongoose.Schema({
   projectIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }],
   sharedAccess: [{ type: mongoose.Schema.Types.ObjectId, ref: "Project" }],
 });
+
 
 const Device = mongoose.models.Device || mongoose.model("Device", deviceSchema);
 const Project =
