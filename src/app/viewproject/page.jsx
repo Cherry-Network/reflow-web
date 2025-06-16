e150f4094a94a9173a97c6387e1a60b3fa1f93a8
Aditya-Coomar <adi.coomar04@gmail.com>
2025-06-16 20:37:38 +0530

    fix: revert changes

--- src/app/viewproject/page.jsx
+++ src/app/viewproject/page.jsx
@@ -206,7 +206,7 @@ const ViewProject = () => {
           return new Date(a.timestamp) - new Date(b.timestamp);
         });
         sortedData?.forEach((data) => {
-          data.timestamp =  formatTimeStampForDisplay(data.timestamp);
+          data.timestamp =  formatTimeStamp(data.timestamp);
         });
         setExportedData(sortedData);
         setReadyToDownload(true);
@@ -426,7 +426,7 @@ const ViewProject = () => {
                   </svg>
                   <span className="text-center">Config</span>
                 </button>
-                <Link
+                {/*<Link
                   href={{
                     pathname: "/viewproject/schedule-reports",
                     query: {
@@ -448,7 +448,7 @@ const ViewProject = () => {
                     <path d="M320-480v-80h320v80H320Zm0-160v-80h320v80H320Zm-80 240h300q29 0 54 12.5t42 35.5l84 110v-558H240v400Zm0 240h442L573-303q-6-8-14.5-12.5T540-320H240v160Zm480 80H240q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h480q33 0 56.5 23.5T800-800v640q0 33-23.5 56.5T720-80Zm-480-80v-640 640Zm0-160v-80 80Z" />
                   </svg>
                   <span className="text-center">Schedule Reports</span>
-                </Link>
+                </Link>*/}
               </div>
               <div className="flex-1 custom-scrollbar">
                 <DataTable
